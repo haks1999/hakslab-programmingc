21813a27ed9b261138b642101cdeef15b7296522
1100413 <1>
2016-08-22 13:36:30 +0900

    간단하게

--- ProgrammingChallenges/src/hackerrank/projectEuler/_003largestPrimeFactor/Solution.java
+++ ProgrammingChallenges/src/hackerrank/projectEuler/_003largestPrimeFactor/Solution.java
@@ -11,43 +11,36 @@ public class Solution {
 
 	public static void main(String[] args){
 
-		List<Long> primeNumbers = new ArrayList<>();
-		primeNumbers.add(2l);
-		primeNumbers.add(3l);
-		primeNumbers.add(5l);
-		primeNumbers.add(7l);
-
-		for( long inx = 11 ; inx <= Math.sqrt(1000000000000l) ; inx++){
-			if( inx % 2 == 0 || inx % 3 == 0 || inx % 5 == 0 || inx % 7 == 0){
-
-			}else{
-				boolean isPrimeNumber = true;
-				for( int jnx = 4 ; jnx < primeNumbers.size() ; jnx++){
-					long curPrimeNumber = primeNumbers.get(jnx);
-					if( curPrimeNumber > Math.sqrt(inx)){
-						break;
-					}
-					if( inx % curPrimeNumber == 0){
-						isPrimeNumber = false;
-						break;
-					}
-				}
-				if( isPrimeNumber ) primeNumbers.add(inx);
-			}
-		}
 		Scanner scanner = new Scanner(System.in);
 
-		int testCasesCount = Integer.parseInt(scanner.nextLine());
-		for( int inx = 0 ; inx < testCasesCount ; inx++){
-			long inputNumber = Long.parseLong(scanner.nextLine());
-			long maxPrimeNumber = 2;
-			for( long primeNumber : primeNumbers ){
-				if( primeNumber > inputNumber ) break;
-				if( inputNumber % primeNumber == 0) {
-					maxPrimeNumber = primeNumber;
+			int testCasesCount = Integer.parseInt(scanner.nextLine());
+			for( int inx = 0 ; inx < testCasesCount ; inx++){
+				long inputNumber = Long.parseLong(scanner.nextLine());
+
+				long curNum = inputNumber;
+				long startNum = 2;
+
+				while(true){
+					if( curNum == 1){
+						break;
+					}
+					if( curNum % startNum == 0 ){
+						curNum = curNum/startNum;
+					}else{
+						startNum++;
+						if(startNum > 3){
+							while(startNum % 2 == 0 || startNum % 3 == 0){
+								startNum++;
+							}
+							if(startNum > Math.sqrt(curNum)){
+								startNum = curNum;
+								break;
+							}
+						}
+					}
 				}
-			}
-			System.out.println(maxPrimeNumber);
+
+			System.out.println(startNum);
 		}
 	}
 }
